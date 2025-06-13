b8a23995bbbb20c7dc38318f60219497680e9825
hunzzo <61585311+hunzzo@users.noreply.github.com>
2025-06-13 15:12:55 +0900

    Refactor to provider-based state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'pages/workout_home_page.dart';
+import 'providers/workout_data.dart';
 
 void main() {
-  runApp(WorkoutApp());
+  runApp(const WorkoutApp());
 }
 
 class WorkoutApp extends StatelessWidget {
+  const WorkoutApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '운동 기록',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (_) => WorkoutData(),
+      child: MaterialApp(
+        title: '운동 기록',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const WorkoutHomePage(),
       ),
-      home: WorkoutHomePage(),
     );
   }
 }
